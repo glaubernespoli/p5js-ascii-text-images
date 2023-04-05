2db8d51671a4d5213206bf31204d5fb34f69e563
Glauber Nespoli <glauber.nespoli@gmail.com>
2023-04-05 16:36:52 +0200

    DOM version

--- main.js
+++ main.js
@@ -8,10 +8,8 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1500, 1500);
-}
+  noCanvas();
 
-function draw() {
   background(0);
   //image(cat, 0, 0, width, height);
 
@@ -19,25 +17,21 @@ function draw() {
   let h = height / cat.height;
   cat.loadPixels();
 
-  for (let i = 0; i < cat.width; i++) {
-    for (let j = 0; j < cat.height; j++) {
+  for (let j = 0; j < cat.height; j++) {
+    let row = "";
+    for (let i = 0; i < cat.width; i++) {
       const pixelIndex = (i + j * cat.width) * 4;
       const r = cat.pixels[pixelIndex + 0];
       const g = cat.pixels[pixelIndex + 1];
       const b = cat.pixels[pixelIndex + 2];
 
       const avg = (r + g + b) / 3;
-
-      noStroke();
-      fill(255);
-      //square(i * w, j * h, w);
-
       const len = density.length;
       const charIndex = floor(map(avg, 0, 255, len, 0));
 
-      textSize(w);
-      textAlign(CENTER, CENTER);
-      text(density.charAt(charIndex), i * w + w * 0.5, j * h + h * 0.5);
+      const c = density.charAt(charIndex);
+      row += c == " " ? "&nbsp" : density.charAt(charIndex);
     }
+    createDiv(row);
   }
 }
